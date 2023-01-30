277bab02340ec73232f12dd53170e788aaae4d4e
Niraj <niraj.patel@flatironschool.com>
2023-01-30 09:38:00 -0500

    Completed

--- lib/seed.py
+++ lib/seed.py
@@ -14,3 +14,33 @@ if __name__ == '__main__':
     engine = create_engine('sqlite:///seed_db.db')
     Session = sessionmaker(bind=engine)
     session = Session()
+
+### This code is for clearing out the database before a seeding. It will delete all data in the data base (not just the seeds)
+# session.query(Game).delete()
+# session.commit()
+
+
+### Hard Code Seeding
+# botw = Game(title="Breath of the Wild", platform="Switch", genre="Adventure", price=60)
+# ffvii = Game(title="Final Fantasy VII", platform="Playstation", genre="RPG", price=30)
+# mk8 = Game(title="Mario Kart 8", platform="Switch", genre="Racing", price=50)
+# ccs = Game(title="Candy Crush Saga", platform="Mobile", genre="Puzzle", price=0)
+
+# session.bulk_save_objects([botw, ffvii, mk8, ccs])
+# session.commit()
+
+
+### Dynamic Randomized Seeding
+print("Seeding games...")
+
+games = [
+    Game(
+        title=fake.name(),
+        genre=fake.word(),
+        platform=fake.word(),
+        price=random.randint(0, 60)
+    )
+for i in range(50)]
+
+session.bulk_save_objects(games)
+session.commit()
